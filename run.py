2654421c5f86b5819144d70e184bbe0a40542151
ryanisnan <ryanisnan@gmail.com>
2018-12-11 21:09:46 -0800

    fix some things, comment some things

--- run.py
+++ run.py
@@ -7,6 +7,10 @@ D4H_API_KEY = os.environ.get('D4H_API_KEY', '')
 
 
 def get_d4h_headers():
+    """
+    Build the header dictionary for the requests library (simply includes the
+    D4H API key).
+    """
     headers = {
         'Authorization': 'Bearer {D4H_API_KEY}'.format(D4H_API_KEY=D4H_API_KEY)
     }
@@ -14,7 +18,14 @@ def get_d4h_headers():
 
 
 def get_member_list():
+    """
+    Query the D4H API and build a list of member objects that we can use.
+    """
     def _convert_status(status):
+        """
+        Helper method that turns D4H's shitty status object into a combination of
+        whether or not the member is operational (True/False), and an optional class (e.g. Resource Member, MIT, etc.)
+        """
         if status['type'].lower() == 'operational':
             operational = True
         elif status['type'].lower() == 'non-operational':
@@ -52,12 +63,15 @@ def get_member_list():
 
     return members
 
-# def filter_active_members(members):
-#     return filter(lambda x: x['status'][''])
-
 
 def render_member_list(members):
-    active_members = sorted(filter(lambda x: x['operational'] is True, members), key=lambda x: x['last_name'])
+    """
+    Render an HTML file with our member data.
+    """
+    def filter_active_members(member):
+        return member['operational'] is True and member['member_class'] != 'Resource Member'
+
+    active_members = sorted(filter(filter_active_members, members), key=lambda x: x['last_name'])
     resource_members = sorted(filter(lambda x: x['member_class'] == 'Resource Member', members), key=lambda x: x['last_name'])
 
     with open('member_list.html') as f:
@@ -69,20 +83,30 @@ def render_member_list(members):
 
 
 def save_html_file(html):
+    """
+    Save the file.
+    """
     with open('rendered_member_list.html', 'w+') as f:
         f.write(html)
 
 
 def main():
+    """
+    Main function that gets run when you run `python run.py`
+    """
     if not D4H_API_KEY:
         raise Exception('bad u')
 
     # Get raw list of users from D4H
     members = get_member_list()
 
+    # Render and save the HTML
     html = render_member_list(members)
     save_html_file(html)
+
+    # Open the file in Firefox, cause I'm lazy
     os.system('firefox rendered_member_list.html')
+
 
 if __name__ == '__main__':
     main()
